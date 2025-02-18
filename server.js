6782ad3709df4c82186280693d3610a84f7c8fe2
aekads <aekads.in@gmail.com>
2025-02-18 16:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.get("/", async (req, res) => {
 
 // Insert Data
 app.post("/add", async (req, res) => {
-    const { company_name, customer_name, customer_email, total_spend, screen_quantity, payment_date, payment_mode, payment_status } = req.body;
-    const sql = "INSERT INTO sales_customer (company_name, customer_name, customer_email, total_spend, screen_quantity, payment_date, payment_mode, payment_status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+    const { company_name, customer_name, customer_email, customer_number, total_spend, screen_quantity, payment_date, payment_mode, payment_status } = req.body;
+    const sql = "INSERT INTO sales_customer (company_name, customer_name, customer_email, customer_number, total_spend, screen_quantity, payment_date, payment_mode, payment_status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
     try {
-        await pool.query(sql, [company_name, customer_name, customer_email, total_spend, screen_quantity, payment_date, payment_mode, payment_status]);
+        await pool.query(sql, [company_name, customer_name, customer_email, customer_number, total_spend, screen_quantity, payment_date, payment_mode, payment_status]);
         res.redirect("/");
     } catch (err) {
         console.error(err);
@@ -42,16 +42,17 @@ app.post("/add", async (req, res) => {
 // Update Data
 app.post("/update/:id", async (req, res) => {
     const { id } = req.params;
-    const { company_name, customer_name, customer_email, total_spend, screen_quantity, payment_date, payment_mode, payment_status } = req.body;
-    const sql = "UPDATE sales_customer SET company_name=$1, customer_name=$2, customer_email=$3, total_spend=$4, screen_quantity=$5, payment_date=$6, payment_mode=$7, payment_status=$8 WHERE id=$9";
+    const { company_name, customer_name, customer_email, customer_number, total_spend, screen_quantity, payment_date, payment_mode, payment_status } = req.body;
+    const sql = "UPDATE sales_customer SET company_name=$1, customer_name=$2, customer_email=$3, customer_number=$4, total_spend=$5, screen_quantity=$6, payment_date=$7, payment_mode=$8, payment_status=$9 WHERE id=$10";
     try {
-        await pool.query(sql, [company_name, customer_name, customer_email, total_spend, screen_quantity, payment_date, payment_mode, payment_status, id]);
+        await pool.query(sql, [company_name, customer_name, customer_email, customer_number, total_spend, screen_quantity, payment_date, payment_mode, payment_status, id]);
         res.redirect("/");
     } catch (err) {
         console.error(err);
         res.send("Error updating data");
     }
 });
+
 
 // Start Server
 app.listen(3000, () => {
